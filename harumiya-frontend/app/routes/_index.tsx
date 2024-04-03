59e0a39c48a712b6756a623edca33344b4566958
Linden Chiu <hchiu87@gmail.com>
2024-04-03 15:44:05 -0700

    skeleton

--- harumiya-frontend/app/routes/_index.tsx
+++ harumiya-frontend/app/routes/_index.tsx
@@ -3,14 +3,14 @@ import type { MetaFunction } from "@remix-run/node";
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { name: "description", content: "Welcome to Harumiya!" },
   ];
 };
 
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Harumiya!</h1>
       <ul>
         <li>
           <a
@@ -19,15 +19,6 @@ export default function Index() {
             rel="noreferrer"
           >
             15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
           </a>
         </li>
         <li>
---
+++ harumiya-frontend/app/routes/world_overview.tsx
@@ -0,0 +1,4 @@
+import { useState } from "react";
+export default function WorldOverview() {
+  const [worldParams, setWorldParams] = useState({});
+}
