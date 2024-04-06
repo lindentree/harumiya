44e5fdda8d85ae8abd6e812856f6d4770822e14b
Linden Chiu <lindentree@Lindens-MacBook-Pro.local>
2024-04-05 22:32:24 -0700

    skeleton

---
+++ src/create_world.rs
@@ -0,0 +1,52 @@
+use std::env;
+
+use gcp_auth::AuthenticationManager;
+
+pub async fn create_world_controller() -> Result<(), Box<dyn std::error::Error>> {
+    let api_endpoint = env::var("API_ENDPOINT")?;
+    let project_id = env::var("PROJECT_ID")?;
+    let location_id = env::var("LOCATION_ID")?; // Sometimes called "region" in gCloud docs.
+
+    let endpoint_url = format!(
+        "https://{api_endpoint}/v1beta1/projects/{project_id}/locations/{location_id}/publishers/google/models/{MODEL_NAME}:generateContent"
+    );
+
+    let authentication_manager = AuthenticationManager::new().await?;
+    let scopes = &["https://www.googleapis.com/auth/cloud-platform"];
+    let token = authentication_manager.get_token(scopes).await?;
+
+    let prompt = "Why is the sky blue?";
+
+    let payload = GenerateContentRequest {
+        contents: vec![Content {
+            role: "user".to_string(),
+            parts: vec![Part::Text(prompt.to_string())],
+        }],
+        generation_config: Some(GenerationConfig {
+            max_output_tokens: Some(2048),
+            temperature: Some(0.4),
+            top_p: Some(1.0),
+            top_k: Some(32),
+            ..Default::default()
+        }),
+        tools: None,
+    };
+
+    let resp = reqwest::Client::new()
+        .post(&endpoint_url)
+        .bearer_auth(token.as_str())
+        .json(&payload)
+        .send()
+        .await?;
+
+    let response = resp.json::<GenerateContentResponse>().await?;
+    response.candidates.iter().for_each(|candidate| {
+        candidate.content.parts.iter().for_each(|part| {
+            if let Part::Text(text) = part {
+                print!("{}", text);
+            }
+        });
+    });
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod create_world;
+
 fn main() {
     println!("Hello, world!");
+
+    create_world::create_world_controller().await;
 }
